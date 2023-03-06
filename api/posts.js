0455ab0b5c6675bd1f1cd6e0bad2b2073716653b
victor <vvazquez307@gmail.com>
2023-03-06 13:39:12 -0600

    post /api/posts

--- api/posts.js
+++ api/posts.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const postsRouter = express.Router();
-
 const morgan = require('morgan');
-const { getAllPosts } = require('../db');
+const { getAllPosts, createPost } = require('../db');
+const { requireUser } = require('./utils');
 
 postsRouter.use(morgan('dev'));
 postsRouter.use(express.json())
@@ -12,6 +12,28 @@ postsRouter.use((req, res, next) => {
   
     next();
 });
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+    const { title, content, tags = "" } = req.body;
+
+    const tagArr = tags.trim().split(/\s+/);
+    const postData = {
+            authorId: req.user.id,
+            title: title,
+            content: content
+        };
+    if (tagArr.length) {
+        postData.tags = tagArr;
+    }
+    try{
+        const post = await createPost(postData);
+
+        res.send( post );
+    }catch({name, message}){
+        next({name, message})
+    }
+  });
+
 
 postsRouter.get('/', async (req, res) => {
     const posts = await getAllPosts();
